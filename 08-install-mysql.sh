a890a06603521ee2a370a61d282561e6a2aacb02
shwagdargi <shwagdargi@gmail.com>
2023-11-03 13:58:10 +0530

    hello array added

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -7,7 +7,7 @@ then
     echo "Error :: Please run this script with root user"
     exit 1
 fi
-yum install mysqlll -y
+yum install mysql -y
 # it is our responsibility again to check installation is success or not
 #need to check exit status
 # exit status is 0 means previous command is success 
